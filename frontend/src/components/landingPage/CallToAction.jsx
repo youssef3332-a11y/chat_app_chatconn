be04bb3a0793e92cee315c27b3839ee0a76b0c0b
youssef3332-a11y <youssef17102003@gmail.com>
2025-01-21 11:43:32 +0100

    adding emailjs logic

--- frontend/src/components/landingPage/CallToAction.jsx
+++ frontend/src/components/landingPage/CallToAction.jsx
@@ -35,13 +35,13 @@ const CallToAction = () => {
 
   return (
     <section id='cta' className='bg-brightRed'>
-      {/* Flex Container */}
+      
       <div className='container flex flex-col items-center justify-between px-6 py-24 mx-auto space-y-12 md:py-12 md:flex-row md:space-y-0'>
-        {/* Heading */}
+        
         <h2 className='text-5xl font-bold leading-tight text-center text-white md:text-4xl md:max-w-xl md:text-left'>
           Contact Us
         </h2>
-        {/* Form */}
+       
         <form onSubmit={handleSubmit} className='flex flex-col space-y-4'>
           <input
             type='text'
