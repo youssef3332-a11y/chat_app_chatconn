0ce9210514a8e8868e1b2a534965b78f576b4a58
youssef3332-a11y <youssef17102003@gmail.com>
2025-01-21 14:23:05 +0100

    add final corrections in CallToAction component

--- frontend/src/components/landingPage/CallToAction.jsx
+++ frontend/src/components/landingPage/CallToAction.jsx
@@ -14,7 +14,7 @@ const CallToAction = () => {
       email,
       message,
     };
-
+{/*final correction*/}
     emailjs.send(
       process.env.REACT_APP_EMAILJS_SERVICE_ID,
       process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
