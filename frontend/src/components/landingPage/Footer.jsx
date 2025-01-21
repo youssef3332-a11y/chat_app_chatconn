e48609df860fb201b4f444d66b67fbf4228f53b1
youssef3332-a11y <youssef17102003@gmail.com>
2025-01-21 11:42:42 +0100

    finichinf the foot component

--- frontend/src/components/landingPage/Footer.jsx
+++ frontend/src/components/landingPage/Footer.jsx
@@ -11,12 +11,12 @@ const Footer = () => {
     <div className='bg-veryDarkBlue'>
       {/* Flex Container */}
       <div className='container flex flex-col-reverse justify-between px-6 py-10 mx-auto space-y-8 md:flex-row md:space-y-0'>
-        {/* Logo and social links container */}
+        
         <div className='flex flex-col-reverse items-center justify-between space-y-12 md:flex-col md:space-y-0 md:items-start'>
           <div className='mx-auto my-6 text-center text-white md:hidden'>
             Copyright © {currentYear}, All Rights Reserved
           </div>
-          {/* Logo and Title */}
+          
           <div className='flex items-center space-x-2 pt-2'>
             <img src={companyLogo} alt='Company Logo' className='h-8' /> {/* Adjust the height to make the logo smaller */}
             <span className='text-white text-2xl font-bold'>ChatConn</span> {/* Add "Conn" to "Chat" */}
