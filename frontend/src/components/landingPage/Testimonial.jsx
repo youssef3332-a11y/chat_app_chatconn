a3e03d98f7380ece93923541d491a2394b9b576c
youssef3332-a11y <youssef17102003@gmail.com>
2025-01-21 14:42:59 +0100

    refactor: update Testimonial component images and content

--- frontend/src/components/landingPage/Testimonial.jsx
+++ frontend/src/components/landingPage/Testimonial.jsx
@@ -1,16 +1,11 @@
-import { Link } from 'react-router-dom';
-
-import reactLogo from '../../assets/images/React-icon.svg.png';
-import expresslogo from '../../assets/images/expresslogo.png';
-import nodejs from '../../assets/images/nodejslogo.png';
+import reactLogo from '../../assets/images/reactLogo.png';
+import expressLogo from '../../assets/images/expressLogo.png';
 import mongodblogo from '../../assets/images/mongodblogo.png';
 
 const Testimonial = () => {
   return (
     <section id='features'>
-      
       <div className='max-w-6xl px-5 mx-auto mt-32 text-center'>
-        
         <h2 className='text-4xl font-bold text-center'>
           Features
         </h2>
@@ -18,7 +13,7 @@ const Testimonial = () => {
         <div className='flex flex-col mt-24 md:flex-row md:space-x-6'>
           {/* Card 1 */}
           <div className='flex flex-col items-center p-6 space-y-6 rounded-lg bg-veryLightGray md:w-1/3'>
-            <img src={reactLogo} className='w-16 -mt-14' alt='' />
+            <img src={reactLogo} className='w-16 -mt-14' alt='React Logo' />
             <h5 className='text-lg font-bold'>React</h5>
             <p className='text-sm text-darkGrayishBlue'>
               React is a free and open-source front-end JavaScript library that aims to make building user interfaces based on components more "seamless".
@@ -27,7 +22,7 @@ const Testimonial = () => {
 
           {/* Card 2 */}
           <div className='hidden flex-col items-center p-6 space-y-6 rounded-lg bg-veryLightGray md:flex md:w-1/3'>
-            <img src={expresslogo} className='w-16 -mt-14' alt='' />
+            <img src={expressLogo} className='w-16 -mt-14' alt='ExpressJS Logo' />
             <h5 className='text-lg font-bold'>ExpressJS</h5>
             <p className='text-sm text-darkGrayishBlue'>
               ExpressJS a back end web application framework for building RESTful APIs with Node. js
@@ -36,30 +31,21 @@ const Testimonial = () => {
 
           {/* Card 3 */}
           <div className='hidden flex-col items-center p-6 space-y-6 rounded-lg bg-veryLightGray md:flex md:w-1/3'>
-            <img src={nodejs} className='w-16 -mt-14' alt='' />
-            <h5 className='text-lg font-bold'>NodeJS</h5>
+            <img src={mongodblogo} className='w-16 -mt-14' alt='MongoDB Logo' />
+            <h5 className='text-lg font-bold'>MongoDB</h5>
             <p className='text-sm text-darkGrayishBlue'>
-              Node.js is an open-source and cross-platform JavaScript runtime environment.
+              MongoDB is a source-available cross-platform document-oriented database program. Classified as a NoSQL database program, MongoDB uses JSON-like documents with optional schemas.
             </p>
           </div>
 
           {/* Card 4 */}
           <div className='hidden flex-col items-center p-6 space-y-6 rounded-lg bg-veryLightGray md:flex md:w-1/3'>
-            <img src={mongodblogo} className='w-16 -mt-14' alt='' />
-            <h5 className='text-lg font-bold'>MongoDB</h5>
+            <img src={nodejsLogo} className='w-16 -mt-14' alt='Node.js Logo' />
+            <h5 className='text-lg font-bold'>Node.js</h5>
             <p className='text-sm text-darkGrayishBlue'>
-              MongoDB is a non-relational document database that provides support for JSON-like storage.
+              Node.js is a JavaScript runtime built on Chrome's V8 JavaScript engine. It is used to build scalable network applications.
             </p>
           </div>
-        </div>
-        {/* Button */}
-        <div className='my-16'>
-          <Link
-            to='#'
-            className='p-3 px-6 pt-2 text-white bg-orange-500 rounded-full baseline hover:bg-orange-600'
-          >
-            Get Started
-          </Link>
         </div>
       </div>
     </section>
